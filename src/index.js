f395374bcd5fc386aed3265f78b998d2ea39a990
Дмитрий <daniel24111993@mail.ru>
2019-10-06 02:20:21 +0300

    first attempt

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+var bracketsObject = {};
+var bracketsArray = [];
+
+for (var i = 0; i < bracketsConfig.length; i++) {
+    bracketsObject[bracketsConfig[0]] = bracketsConfig[1];
 }
+
+var firstBracket = str[0];
+
+if (bracketsObject[firstBracket] === false) {
+    return false;
+} else {
+  bracketsArray.push(firstBracket);
+}
+
+for (var j = 1; j < str.length; j++) {
+    if (bracketsObject[str[j]] === true) {
+      bracketsObject.push(str[j]);
+    } else {
+        if (bracketsObject[str[j - 1]] === str[j]) {
+            bracketsObject.pop(str[j - 1]);
+        } else {
+            return false;
+        }
+    }
+  }
+};
+
+
+
+  
+
